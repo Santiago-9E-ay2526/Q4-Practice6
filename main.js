710346e995a12fe6c5898737e9eb34cef35252e3
Santiago <santiago.mj@obmontessori.edu.ph>
2026-04-24 10:04:41 +0800

    added code

---
+++ main.js
@@ -0,0 +1,6 @@
+function animalCount () {
+     var animals = ["Lion", "Flamingo", "Polar Bear", "Boa Constrictor"];
+          for (var i = 0; i < animals.length; i++) {
+          window.alert("This zoo contains a " + animals[i] + ".");
+     }
+}
